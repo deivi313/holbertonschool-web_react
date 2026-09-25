9bdc2b6e7b95aab5d2bf13fc1e50aeb52fc0b689
deivi313 <deiviboja@gmail.com>
2026-09-25 17:44:56 +0200

    task 3

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -15,12 +15,12 @@ const App = () => {
 
       <div className="App-body">
         <p>Login to access the full dashboard</p>
-        <label>
-          <input type="email" name="email" id="email" />
-        </label>
-        <label>
-          <input type="password" name="password" id="password" />
-        </label>
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email" id="email" />
+
+        <label htmlFor="password">Password: </label>
+        <input type="password" name="password" id="password" />
+
         <button>OK</button>
       </div>
 
