eb9d3856851fd07ee5de91911b8a2d28ea265662
deivi313 <deiviboja@gmail.com>
2026-09-25 16:23:45 +0200

    task 1 done

---
+++ react_intro/task_0/dashboard/src/utills.js
@@ -0,0 +1,10 @@
+import './Notifications.css'
+
+const Notification = () => {
+  return <div className="notification-items">
+
+    <p>Here is the list of notifications</p>
+
+  </div>
+}
+export default Notification;
