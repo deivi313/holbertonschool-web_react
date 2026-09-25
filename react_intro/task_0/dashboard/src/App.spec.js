28d66f7826aa1f09444baa5caf75950b70799010
deivi313 <deiviboja@gmail.com>
2026-09-25 15:54:44 +0200

    changes made

--- react_intro/task_0/dashboard/src/App.spec.js
+++ react_intro/task_0/dashboard/src/App.spec.js
@@ -1,17 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-
 test("renders h1 with School Dashboard text", () => {
-  render(<App />)
-  expect(screen.getByRole('heading')).toHaveTextContent(/School dashboard/i);
-})
+  render(<App />);
+  const heading = screen.getByRole("heading", { level: 1, name: /school dashboard/i });
+  expect(heading).toBeInTheDocument();
+});
 
 test("renders correct text in body and footer paragraphs", () => {
-  render(<App />)
-  expect(screen.getByText(/Login to access the full dashboard/i));
-  expect(screen.getByText(/Copyright/i));
-})
+  render(<App />);
+
+  const bodyText = screen.getByText(/login to access the full dashboard/i);
+  expect(bodyText).toBeInTheDocument();
+
+  const footerRegex = /copyright \d{4}.*holberton school/i;
+  const footerNode = screen.getByText(footerRegex);
+  expect(footerNode).toBeInTheDocument();
+});
+
 test("renders an image", () => {
   render(<App />);
   const image = screen.getByAltText(/holberton logo/i);
