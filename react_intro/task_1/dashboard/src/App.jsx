d21b011b4f923b655eec36cff49e409aea4697ac
deivi313 <deiviboja@gmail.com>
2026-09-25 16:36:05 +0200

    task 1 done

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -1,12 +1,13 @@
 import logo from "./assets/holberton-logo.jpg";
-
 import "./App.css";
+import Notifications from './Notifications'
 
 const App = () => {
   const date = new Date();
 
   return (
     <>
+      <Notifications />
       <div className="App-header">
         <img src={logo} alt="holberton logo" />
         <h1 style={{ color: "#e1003c" }}>School dashboard</h1>
